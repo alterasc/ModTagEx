e0712679267967d434029fa1d511ccd5613da068
alterasc <alterasc@protonmail.com>
2026-04-03 14:44:37 +0200

    make all settings enabled by default

--- ModTagEx/ModTagExSettings.cs
+++ ModTagEx/ModTagExSettings.cs
@@ -10,8 +10,8 @@ public class ModTagExSettings : UnityModManager.ModSettings
     public bool LevelUpFeature = true;
     public bool UIFeature = true;
     public bool Ability = true;
-    public bool ActivatableAbility = false;
-    public bool Buff = false;
+    public bool ActivatableAbility = true;
+    public bool Buff = true;
     public bool Item = true;
     public string DatabaseCommit = "";
     public long LastUpdateTS;
